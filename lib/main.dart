bdc71a3681de0e23de8de75bd8d87fd76cd79bbe
Bhanu <bhanusoni@jklu.edu.in>
2022-09-19 16:59:43 +0530

    Themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project/consts/theme_data.dart';
+import 'package:flutter_project/provider/dark_theme_provider.dart';
 import 'package:flutter_project/screens/home_screens.dart';
+import 'package:flutter_project/services/dark_theme_pref.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DarkThemeProvider themeChangeProvider = DarkThemeProvider();
+
+  void getCurrentAppTheme() async {
+    themeChangeProvider.setDarkTheme =
+        await themeChangeProvider.darkThemePrefs.getTheme(); //
+  }
+
+  @override
+  void initState() {
+    //TODO: implement init
+    super.initState();
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     bool _isDark = true;
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: Styles.themeData(true, context),
-        home: HomeScreen());
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) {
+          return themeChangeProvider;
+        })
+      ],
+      child:
+          Consumer<DarkThemeProvider>(builder: (context, themeProvider, child) {
+        return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            theme: Styles.themeData(themeProvider.getDarkTheme, context),
+            home: HomeScreen());
+      }),
+    );
   }
 }
